308a89c560a5dd1605bc462e2f678961cadb88e6
tangguowei <1214475704@qq.com>
2021-06-10 16:21:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+<!--
+ * @Description: file content
+ * @Author: tangguowei
+ * @Date: 2021-06-10 16:05:54
+ * @LastEditors: tangguowei
+ * @LastEditTime: 2021-06-10 16:20:13
+-->
+# rack-example-server
+> The local server for the plug-in project named [vue-cli-plugin-rack](https://www.npmjs.com/package/vue-cli-plugin-rack)
+> 
+
+## Project setup
+```
+npm install
+```
+
+### Start
+```
+node ./bin/www
+```
+
+## Links
+
+[front end project](https://github.com/guoweiTang/rack-example-web)
--- app.js
+++ app.js
@@ -3,17 +3,18 @@
  * @Author: tangguowei
  * @Date: 2021-06-09 16:50:52
  * @LastEditors: tangguowei
- * @LastEditTime: 2021-06-10 15:14:59
+ * @LastEditTime: 2021-06-10 16:00:47
  */
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(express.static(__dirname + '/public'));
 app.use(routes);
 
 module.exports = app;
